3b50aa66690a733738f3aef32441958b4ab17a40
gauravpaliwal1 <gauravpaliwal1@users.noreply.github.com>
2018-08-12 15:58:58 +0530

    Initial project add

---
+++ src/main/java/gp/spingframework/gppetclinic/GpPetClinicApplication.java
@@ -0,0 +1,12 @@
+package gp.spingframework.gppetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GpPetClinicApplication.class, args);
+    }
+}
